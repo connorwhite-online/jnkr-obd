27c55fb00c2943228183e103b7ea5146598d5f57
Connor White <connorwhitepdx@gmail.com>
2022-08-02 23:59:23 -0700

    initial

---
+++ glow_plugs.ino
@@ -0,0 +1,11 @@
+// Instantiate applicable libraries here
+
+void.setup() {
+  // Initialize the library
+  Serial.begin(9600);
+  Serial.println("Fire it up");
+}
+
+void.loop() {
+  // Do something every loop
+}
