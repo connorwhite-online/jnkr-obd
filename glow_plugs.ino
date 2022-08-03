c2e4549f46ca43e986df3e7df09a84b09b7a2153
Connor White <connorwhitepdx@gmail.com>
2022-08-03 00:22:37 -0700

    setup scaffolding

--- glow_plugs.ino
+++ glow_plugs.ino
@@ -1,23 +1,32 @@
 // Instantiate applicable libraries here
 
+// Pinout
+int AmbientAir = 3; // Ambient air temp sensor at pin 3
+int Relay = 4; // Relay at pin 4
+
 void.setup() {
   // Initialize the library
   Serial.begin(9600);
   
-  // Temperature sensor port
+  pinMode(AmbientAir, INPUT);
+  pinMode(Relay, OUTPUT);
 
-  // Relay port
+  // Relay port off until actuated
+  digitalWrite(Relay, LOW);
 
 }
 
 void.loop() {
-  // If temp is at or below 70 degrees, turn on the relay
-    int temp = temperatureSensor.read();
-    int threshold = 70;
 
-  if (temp <= threshold) {
-    digitalWrite(relay, HIGH);
-  } else {
-    digitalWrite(relay, LOW);
-  }
+    int threshold = 70; // Threshold temperature for relay activation
+
+    // Read the temperature sensor
+    digitalRead(AmbientAir);
+
+    // If temp is at or below 70 degrees, turn on the relay
+    if (AmbientAir <= threshold) {
+        digitalWrite(Relay, HIGH);
+    } else {
+        digitalWrite(Relay, LOW);
+    }
 }
